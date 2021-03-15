ac2d77b20aabef5b21d78cf1b33256c4251090fb
Travis CI <najwer23@gmail.com>
2021-03-15 22:48:46 +0100

    Refractoring

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -8,7 +8,7 @@ const objMenuLinks = {
     '/contact': 'Contact',
     '/gallery': 'Gallery'
 }
-  
+
 window.onload = function () {
     const hamburgerBtn = document.querySelector('.hamburger-button');
     const menuMobile = document.querySelector('.menu-mobile');
@@ -18,13 +18,13 @@ window.onload = function () {
         hamburgerBtn.setAttribute('aria-expanded', hamburgerBtn.classList.contains('hamburger-active'));
         menuMobile.classList.toggle('menu-mobile-active');
         document.body.classList.toggle('scroll');
-        
+
         // TODO later
         // all links on website should has class link
         let arrayLinks = document.getElementsByClassName('link');
         for (let i = 0; i < arrayLinks.length; i++) {
             arrayLinks[i].classList.toggle('hideLink');
-        }     
+        }
     }
 
     function menuMobileHide() {
@@ -44,9 +44,9 @@ function MobileMenu() {
     return (
         <div class="menu-mobile">
             <ul class="menuLinks">
-                {Object.keys(objMenuLinks).map((v,i)=>(
+                {Object.keys(objMenuLinks).map((v, i) => (
                     <li key={i}><a href={v}>{objMenuLinks[v]}</a></li>
-                ))}  
+                ))}
             </ul>
         </div>
     )
@@ -67,21 +67,21 @@ function Hamburger() {
 
 function Nav() {
     return (
-        <section>           
+        <>
             <MobileMenu />
             <div className="menu">
                 <div className="menu-box">
                     <Hamburger />
-                    <div className="menu-title"> <FontAwesomeIcon icon={faMugHot} size="lg"/> Coffee2U </div>
+                    <div className="menu-title"> <FontAwesomeIcon icon={faMugHot} size="lg" /> Coffee2U </div>
                     <ul className="menu-list">
-                        {Object.keys(objMenuLinks).map((v,i)=>(
+                        {Object.keys(objMenuLinks).map((v, i) => (
                             <li key={i}><a href={v}>{objMenuLinks[v]}</a></li>
-                        ))}  
+                        ))}
                     </ul>
                 </div>
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150"><path fill="#6F4E37" fillOpacity="1" d="M0,32L80,32C160,32,320,32,480,48C640,64,800,96,960,106.7C1120,117,1280,107,1360,101.3L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
-        </section>
+        </>
     )
 }
 
