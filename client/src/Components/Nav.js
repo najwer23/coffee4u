1767dfdaea09ff79cf07fd0bdb589b798107797f
Travis CI <najwer23@gmail.com>
2021-03-16 22:30:01 +0100

    Skeleton for Routes for menu

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -2,6 +2,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faMugHot } from '@fortawesome/free-solid-svg-icons'
 import React, { Component } from 'react';
 import '../CSS/Nav.css';
+import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams} from "react-router-dom";
+import Home from './Home';
+
 
 const objMenuLinks = {
     '/': 'Home',
@@ -58,36 +61,45 @@ class Hamburger extends Component {
     }
 }
 
-function MobileMenu() {
-    return (
-        <div className="menu-mobile">   
-            <ul className="menu-mobile-links">
-                <li>Coffee4U</li>
-                {Object.keys(objMenuLinks).map((v, i) => (
-                    <li key={i}><a href={v}>{objMenuLinks[v]}</a></li>
-                ))}
-                <li>Mariusz Najwer <br />2021</li>
-            </ul>
-        </div>
-    )
+class MobileMenu extends Hamburger {
+    render() {
+        return (
+            <div className="menu-mobile">   
+                <ul className="menu-mobile-links">
+                    <li>Coffee4U</li>
+                    {Object.keys(objMenuLinks).map((v, i) => (
+                        <li onClick={this.menuMobileClick} key={i}><Link to={v}>{objMenuLinks[v]}</Link></li>
+                    ))}
+                    <li>Mariusz Najwer <br />2021</li>
+                </ul>
+            </div>
+        )
+    } 
 }
 
 function Nav() {
     return (
         <>
-            <MobileMenu />
-            <div className="menu">
-                <div className="menu-box">
-                    <Hamburger />
-                    <div className="menu-title"> <FontAwesomeIcon icon={faMugHot} size="lg" /> Coffee4U </div>
-                    <ul className="menu-list">
-                        {Object.keys(objMenuLinks).map((v, i) => (
-                            <li key={i}><a href={v}>{objMenuLinks[v]}</a></li>
-                        ))}
-                    </ul>
+            <Router>
+                <MobileMenu />
+                <div className="menu">
+                    <div className="menu-box">
+                        <Hamburger />
+                        <div className="menu-title"> <FontAwesomeIcon icon={faMugHot} size="lg" /> Coffee4U </div>
+                        <ul className="menu-list">
+                            {Object.keys(objMenuLinks).map((v, i) => (
+                                <li key={i}><Link to={v}>{objMenuLinks[v]}</Link></li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150"><path fill="#6F4E37" fillOpacity="1" d="M0,32L80,32C160,32,320,32,480,48C640,64,800,96,960,106.7C1120,117,1280,107,1360,101.3L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150"><path fill="#6F4E37" fillOpacity="1" d="M0,32L80,32C160,32,320,32,480,48C640,64,800,96,960,106.7C1120,117,1280,107,1360,101.3L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>   
+                <Switch>
+                    <Route path="/drinks">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
         </>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams} from "react-router-dom";
 import './CSS/index.css';
 import './CSS/muliFont.css';
 import Nav from './Components/Nav';
