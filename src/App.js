b849fafbb5f6177ccf2c0fc27bf826f6f76f8330
Travis CI <najwer23@gmail.com>
2021-03-19 14:38:02 +0100

    New colors for web, definition of coffee

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Footer from './Components/Footer';
 import Nav from './Components/Nav';
-
+import './CSS/index.css';
 
 function App() {
     return (
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,41 @@
+import '../CSS/Home.css';
+import Aos from 'aos';
+import "aos/dist/aos.css";
+import { useEffect } from "react"
+
+function InitAOS () {
+    useEffect(() => {
+        Aos.init({duration: 1000});
+    }, []);    
+}
+
 function Home() {
+    InitAOS()
+
     return (
-        <h1>42</h1>
+        <article>
+        <section className="ss">
+            <h1 data-aos="fade-right">Life is too short for bad coffee...</h1>
+            <h2 data-aos="fade-left">...but even a bad cup of coffee is better than no coffee at all.</h2>
+        </section>
+
+        <section className="ss">
+            <img className="ss-img" src={`${process.env.PUBLIC_URL}/assets/images/h1.jpg`} alt="img about"/>
+            <div className="ss2">
+                <h2>What is coffee?!</h2>
+                <p> 
+                    <ol>
+                        <li>Coffee [noun] - a magic potion that allows human muscles to move, especially on Monday mornings.</li>
+                        <li>Coffee [noun] - a dark, magical substance that turns "leave me alone" into "good morning, honey".</li>
+                        <li>Coffee [noun] - nectar of the Gods.</li>
+                        <li>Coffee - Community of Faith for Economic Empowerment</li>
+                    </ol>
+                </p>
+                <p>We have also boring, formal definition: Coffee - <i><b>a hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub.</b></i></p>
+            </div>
+        </section>
+        </article>
+
     )
 }
 
